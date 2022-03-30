d64b84be768ea2b42ca3c35cd973400f3a276cfb
AMPA97 <94563250+AMPA97@users.noreply.github.com>
2022-03-30 13:05:29 -0600

    Add files via upload

---
+++ read.py
@@ -0,0 +1,32 @@
+def read():
+    CP=[]
+    with open("./archivos/CP.txt", "r") as f:
+        next(f, None)
+        next(f, None)    
+        for line in f :
+            lista=line.split("|")
+            dic={
+                'Codigo_postal':lista[0],
+                'nombre_acentamiento':lista[1],
+                'tipo_acentamiento':lista[2],
+                'municipio':lista[3],
+                'estado':lista[4],
+                'ciudad':lista[5],
+                'd_CP':lista[6],
+                'c_estado':lista[7],
+                'c_oficina':lista[8],
+                'c_CP':lista[9],
+                'c_tipo_asenta':lista[10],
+                'c_mnpio':lista[11],
+                'id_asenta_cpcons':lista[12],
+                'tipo_zona':lista[13],
+                'c_cve_ciuda':lista[14]
+            }
+            CP.append(dic)  
+
+    print(CP)
+
+def run():
+    read()
+if __name__=="__main__":
+    run() 
